d9919e4e4d7b39e27d432192a971f9cc06f128ca
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2023-08-18 16:51:22 +0900

    feat: Add sample

--- cmd/rueidis/main.go
+++ cmd/rueidis/main.go
@@ -13,9 +13,30 @@ func main() {
 	}
 	defer client.Close()
 
-	ctx := context.Background()
-	// ping
-	if err := client.Do(ctx, client.B().Ping().Build()).Error(); err != nil {
+	if err := test(client); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func test(client rueidis.Client) error {
+	ctx := context.Background()
+	var err error
+	// PING
+	err = client.Do(ctx, client.B().Ping().Build()).Error()
+	if err != nil {
+		return err
+	}
+	// SET
+	err = client.Do(ctx, client.B().Set().Key("key").Value("value").Build()).Error()
+	if err != nil {
+		return err
+	}
+	// GET
+	v, err := client.Do(ctx, client.B().Get().Key("key").Build()).ToString()
+	if err != nil {
+		return err
+	}
+	log.Printf("GET key: %s", v)
+
+	return nil
 }
