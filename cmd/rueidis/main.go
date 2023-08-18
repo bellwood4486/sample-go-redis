48ce1725cdaffe23c8ef9528d5b45c438edc273b
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2023-08-18 16:55:34 +0900

    style

--- cmd/rueidis/main.go
+++ cmd/rueidis/main.go
@@ -7,7 +7,11 @@ import (
 )
 
 func main() {
-	client, err := rueidis.NewClient(rueidis.ClientOption{InitAddress: []string{"127.0.0.1:6379"}})
+	client, err := rueidis.NewClient(
+		rueidis.ClientOption{
+			InitAddress: []string{"127.0.0.1:6379"},
+		},
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
